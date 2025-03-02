047a40294ef6be6e69414a6a02780a7a42eee3ea
vladislav602 <romanpavelko70@gmail.com>
2025-03-02 15:29:45 +0200

    2
    1

--- main.py
+++ main.py
@@ -1,48 +1,33 @@
-from PyQt6.QtWidgets import*
-
+from PyQt6.QtWidgets import *
 
 app = QApplication([])
-
 window = QWidget()
 
+pole = QTextEdit()
+notes_list_lbl = QLabel("Cписок заміток")
+notatku_list = QListWidget()
+add_note_btn = QPushButton("Додати замітку")
+delete_note_btn = QPushButton("Видалити")
+save_note_btn = QPushButton("Зберегти")
 
-
+tag_list_lbl = QLabel("Cписок заміток")
+tags_list = QListWidget()
+tag_input = QLineEdit()
 main_line = QHBoxLayout()
+main_line.addWidget(pole)
 
+v1 = QVBoxLayout()
+v1.addWidget(notes_list_lbl)
+v1.addWidget(notatku_list)
+v1.addWidget(save_note_btn)
+v1.addWidget(delete_note_btn)
+v1.addWidget(add_note_btn)
+v1.addWidget(tag_list_lbl)
+v1.addWidget(tags_list)
+v1.addWidget(tag_input)
+main_line.addLayout(v1)
 
-spusokzamitok_lbl = QLabel("Список заміток")
-
-pole_txt = QTextEdit()
-notes_list = QListWidget()
-create_note_btn =QPushButton("Створити замітку")
-delete_note_btn =QPushButton("Видалити замітку")
-
-main_line.addWidget(pole_txt)
-
-A1 = QVBoxLayout()
-A1.addWidget(spusokzamitok_lbl)
-A1.addWidget(notes_list)
-A1.addWidget(create_note_btn)
-A1.addWidget(delete_note_btn)
-
-spusoktegiv_lbl = QLabel("Список тегів")
-pole_txt2 = QTextEdit()
-teg_list = QLineEdit()
-add_to_note_btn = QPushButton("Додати до замітки")
-open_from_note_btn = QPushButton("Відкріпити від замітки")
-search_for_teg_note_btn = QPushButton("Шукати замітки до тегу")
-
-main_line.addWidget(pole_txt2)
-A1 = QVBoxLayout()
-A1.addWidget(spusoktegiv_lbl)
-A1.addWidget(teg_list)
-A1.addWidget(add_to_note_btn)
-A1.addWidget(open_from_note_btn)
-A1.addWidget(search_for_teg_note_btn)
 
 window.setLayout(main_line)
-main_line.addLayout(A1)
-
-
 window.show()
 app.exec()
